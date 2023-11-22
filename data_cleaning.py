228246ab02fc7d9e6e5e8cf4d9fa827324903eca
Giovanni Briglia <93917046+Giovannibriglia@users.noreply.github.com>
2023-11-22 19:43:47 +0100

    update

--- data_cleaning.py
+++ data_cleaning.py
@@ -8,7 +8,7 @@ from scipy import stats, signal
 from scipy.fftpack import fft, fftfreq
 warnings.filterwarnings('ignore')
 
-sample_rate_cutted = 25000
+sample_rate = 25600
 seconds_of_acquistion = 10
 path_inputs = 'Vibr_MONO_NO_offset'
 path_saving = 'dataframes_mono'
@@ -17,7 +17,7 @@ labelsize = 12
 
 
 def notch_filter(vet, notch_freq, quality):
-    b_notch, a_notch = signal.iirnotch(notch_freq, quality, sample_rate_cutted)
+    b_notch, a_notch = signal.iirnotch(notch_freq, quality, sample_rate)
 
     outputSignal = signal.filtfilt(b_notch, a_notch, vet)
 
@@ -25,8 +25,8 @@ def notch_filter(vet, notch_freq, quality):
 
 
 def FFT(vet, time_in_sec):
-    n = sample_rate_cutted * time_in_sec
-    T = 1 / 25600  # 25600 is the real sample rate
+    n = sample_rate * time_in_sec
+    T = 1 / sample_rate
     y = vet
     yf = fft(y)
     yfl = 2 * abs(yf[:n // 2] / n)
@@ -65,8 +65,10 @@ for max_frequency in [375, 125, 75]:
         df = pd.DataFrame([[0] * len(features)], columns=features)
 
         row_number = 0
-        half_sample_rate = int(sample_rate_cutted / 2)
+        half_sample_rate = int(sample_rate / 2)
         values_for_fft_plots = []
+
+
         for filename in glob.glob(f"{path_inputs}\*.csv"):
             with open(os.path.join(os.getcwd(), filename), "r") as f:
                 data = pd.read_csv(filename)
@@ -113,6 +115,8 @@ for max_frequency in [375, 125, 75]:
                     yfl, xfl = FFT(vet=vet_Notch, time_in_sec=1)
                     if count == half_sample_rate and (indexR + indexT == 2):
                         values_for_fft_plots.append(yfl)
+                        yfl_wrong, xfl_wrong = FFT(vet=input_data, time_in_sec=1)
+                        values_for_fft_plots.append(yfl_wrong)
 
                     if if_vis:
                         yfl_wrong, xfl_wrong = FFT(vet=input_data, time_in_sec=1)
@@ -177,18 +181,25 @@ for max_frequency in [375, 125, 75]:
                     df.loc[row_number] = row
                     row_number += 1
 
-        fig_subplots, axes = plt.subplots(3, 1, sharex=True, dpi=600)
-        x = np.arange(0, 500, 1)
-        azz = [': Healthy case', ': Damage on the Outer Ring', ': Brinnelling Damage']
-        for val in range(len(values_for_fft_plots)):
-            axes[val].plot(x, values_for_fft_plots[val][:500], linewidth=2)
-            axes[val].set_title(f'D{val + 1}-R{indexR}-T{indexT} {azz[val]}', fontsize=fontsize + 5)
-            axes[val].set_ylabel('Amplitude', fontsize=fontsize)
-            axes[val].grid(True)
-        axes[len(values_for_fft_plots) - 1].set_xlabel('Hz', fontsize=fontsize)
-        plt.tight_layout()
-        # plt.savefig('ResultingSignal_CURR2.pdf')
-        plt.show()
+        if max_frequency == 375:
+            fig_subplots, axes = plt.subplots(3, 1, sharex=True, dpi=600)
+            x = np.arange(0, 500, 1)
+            azz = [': Healthy case', ': Damage on the Outer Ring', ': Brinnelling Damage']
+            for val in range(0, len(values_for_fft_plots), 2):
+                if int(val / 2) == 0:
+                    axes[int(val / 2)].plot(x, values_for_fft_plots[val][:500], linewidth=2, label='After Notch')
+                    axes[int(val / 2)].plot(x, values_for_fft_plots[val + 1][:500], linewidth=1, label='Before Notch')
+                    axes[int(val / 2)].legend(loc='best')
+                else:
+                    axes[int(val / 2)].plot(x, values_for_fft_plots[val][:500], linewidth=2)
+                    axes[int(val / 2)].plot(x, values_for_fft_plots[val + 1][:500], linewidth=1)
+
+                axes[int(val/2)].set_title(f'D{int(val/2) + 1}-R{indexR}-T{indexT} {azz[int(val/2)]}', fontsize=fontsize + 5)
+                axes[int(val/2)].set_ylabel('Amplitude', fontsize=fontsize)
+                axes[int(val/2)].grid(True)
+            plt.tight_layout()
+            plt.savefig('ResultingSignal_Mono.pdf')
+            plt.show()
 
         to_del = []
         for i in range(1, len(features) - 1, 1):
